8efa227874d3b75eb558885983e91bc76c6ada3e
Pranav V R <pvrforpranavvr@gmail.com>
2018-02-14 21:45:40 +0530

    Added gitignore, executable build file

---
+++ build.cmd
@@ -0,0 +1,7 @@
+call mvn clean install
+call mvn clean package 
+echo	  ____   __     __  ____  
+echo 	|  _ \  \ \   / / |  _ \ 
+echo 	| |_) |  \ \ / /  | |_) |
+echo 	|  __/    \ V /   |  _ < 
+echo 	|_|        \_/    |_| \_\
---
+++ build.sh
@@ -0,0 +1,10 @@
+mvn clean install
+mvn clean package
+cat << "EOF" 
+  ____   __     __  ____  
+ |  _ \  \ \   / / |  _ \ 
+ | |_) |  \ \ / /  | |_) |
+ |  __/    \ V /   |  _ < 
+ |_|        \_/    |_| \_\
+                          
+EOF
