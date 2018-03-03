b6e2cb548df5fc7c5a107ceaf74ab73e888bd733
Pranav V R <pvrforpranavvr@gmail.com>
2018-03-03 22:45:09 +0530

    Springfox swagger implementaion

--- src/main/java/com/sh/configurations/AppConfig.java
+++ src/main/java/com/sh/configurations/AppConfig.java
@@ -1,20 +1,22 @@
 package com.sh.configurations;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configurable
+@Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.sh")
 @PropertySource("classpath:application.properties")
-public class AppConfig {
-	@Autowired
-	Environment env;
-	
-	
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 
 }
